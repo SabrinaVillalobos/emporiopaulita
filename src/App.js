6457061f65eb3ebc565e16e736f2ec3cb40418ff
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-04-03 16:11:28 -0300

    creando ruta de registro con e-mail

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import RegisterWithEmail from './components/RegisterWithEmail';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Switch>
+          <Route exact path="/register-email" component={RegisterWithEmail} />
+        </Switch>
       </div>
     );
   }
