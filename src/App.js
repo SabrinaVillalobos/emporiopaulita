4ac35a1587fc4d8e5aceb78192acd9b4a505261d
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-04-03 17:49:57 -0300

    añadiendo navbar y más rutas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
+import Navbar from './components/Navbar';
 import RegisterWithEmail from './components/RegisterWithEmail';
 import './App.css';
 
@@ -8,6 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/register-email" component={RegisterWithEmail} />
