9ac86e112b5319087a8cb01fb288e8ed3e4aff8d
SabrinaVillalobos <wafflish@gmail.com>
2018-04-04 12:42:15 -0300

    login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -14,26 +14,26 @@ class Login extends Component {
   }
   render() {
     return (
-      <div className="row form-user" id="Login">
+      <div className="row" id="Login">
         <div className="col-sm">
-          <div className="logo-container"><img src={Logo} alt="Logo del Emporio" id="LoginLogo" className="logo"/></div>
-          <p>Sabemos lo que quieres
-            y cómo lo quieres
+          <div id="LogoContainer"><img src={Logo} alt="Logo del Emporio" id="LoginLogo"/></div>
+          <p className="weknow">Sabemos lo que quieres</p>
+            <p className="andhow">y cómo lo quieres
           </p>
           <form>
             <div className="form-group">
-              <label htmlFor="LoginMail">Inicia Sesión</label>
-              <input type="email" value={this.state.email} className="form-control" id="LoginMail" placeholder="Correo Electrónico" onChange={e => {
+              <label htmlFor="LoginMail" className="login">Inicia Sesión</label><hr className="line" />
+              <input className="mail" type="email" value={this.state.email} className="form-control" id="LoginMail" placeholder="Correo Electrónico" onChange={e => {
                 this.setState({
                   email: e.target.value
                 })}}/>
-              <input type="password" value={this.state.pass} className="form-control" id="LoginPass" placeholder="Contraseña" onChange={e => {
+              <input type="password" value={this.state.pass} className="form-control" id="PassMail" placeholder="Contraseña" onChange={e => {
                 this.setState({
                   pass: e.target.value
                 })
               }}/>
             </div>
-            <button type="submit" className="btn btn-primary" onClick={e => {
+            <div className="text-center"><button type="submit" className="btn continuebtn btn-primary" onClick={e => {
               e.preventDefault();
               let email = this.state.email;
               let pass = this.state.pass;
@@ -42,16 +42,16 @@ class Login extends Component {
                   this.setState({
                     email: '',
                     pass: ''
-                  });
+                  })
                 });
-                window.location = '/';
-            }}>Continuar</button>
+            }}>Continuar</button></div>
             <Link className="missing-pass" to="/contra">¿Olvidaste tu contraseña?</Link>
-            <Link className="missing-pass" to="/register">Registrate</Link>
           </form>
           <p>----o-----</p>
           <button>Continuar con Google</button>
           <button>Continuar con Facebook</button>
+          <p className="noacc">¿No tienes una cuenta?</p>
+          <Link className="missing-pass" to="/register">REGISTRATE</Link>
           <button onClick={() => auth.doSignOut()}>Salir</button>
         </div>
       </div>
